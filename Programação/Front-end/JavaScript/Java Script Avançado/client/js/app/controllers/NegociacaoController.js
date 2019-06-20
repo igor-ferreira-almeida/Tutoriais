dbea333533b49effb43a97d92cfdabb1ee35a779
igor-ferreira-almeida <igor.rigel@gmail.com>
2019-06-20 17:12:32 -0300

    Java Script Avançado parte II

--- Programação/Front-end/JavaScript/Java Script Avançado/client/js/app/controllers/NegociacaoController.js
+++ Programação/Front-end/JavaScript/Java Script Avançado/client/js/app/controllers/NegociacaoController.js
@@ -9,7 +9,23 @@ class NegociacaoController {
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
 
-    this._listaNegociacoes = new ListaNegociacoes(model => this._negociacaoView.update(model));
+    let self = this;
+    this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+      get(target, prop, receiver) {
+
+          if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop] == typeof(Function))) {
+              return function() {
+                  console.log(`interceptando ${prop}`);
+                  Reflect.apply(target[prop], target, arguments);
+                  self._negociacaoView.update(target);
+              }
+          } 
+          return Reflect.get(target, prop, receiver);
+      }
+  });
+
+
+
 
     this._negociacaoView = new NegociacaoView($("#negociacoesView"));
     this._mensagem = new Mensagem();
